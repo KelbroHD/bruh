6687587f3e4f7dbcda8a52c5a5c959771bdc84aa
Saijen <113825306+KelbroHD@users.noreply.github.com>
2023-06-05 20:17:16 +0700

    bruh

---
+++ righttriangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int x = 5;
+
+    for (int i = 0; i < x; i++){
+
+        for(int n = 0; n < i; n++){
+
+            
+            printf("#");
+
+            }
+        printf("#\n");
+    }
+
+    }
